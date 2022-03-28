0be6644fb7dc64256a661429e379b6f670f09f84
Simen Klemp <simenk2312@gmail.com>
2022-03-28 14:38:51 +0200

    Major revision in SubjectQueueForm.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
 import store from "./store";
+import router from "./router";
 
-createApp(App).use(store).use(router).mount("#app");
+const requireComponent = require.context(
+    "./components",
+    false,
+    /Base[A-Z]\w+.(vue|js)$/
+);
+const app = createApp(App);
+
+requireComponent.keys().forEach((fileName) => {
+    const componentConfig = requireComponent(fileName);
+
+    const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+);
+    app.component(componentName, componentConfig.default || componentConfig);
+});
+
+app.use(store).use(router).mount("#app");
+
+
