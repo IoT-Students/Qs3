6e48e5aaf43735413d5316a74b277501f5889d99
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-28 13:12:16 +0200

    Created SubjectCard.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="about">
     <h1>Home page</h1>
-    <HomeComponent> </HomeComponent>
+    <SubjectCard
+      v-for="subject in subjects"
+      :key="subject.id"
+      :subject="subject"
+    >
+    </SubjectCard>
   </div>
 </template>
 
 <script>
-import HomeComponent from "../components/HomeComponent";
+import SubjectCard from "@/components/SubjectCard";
 
 export default {
   name: "Login",
   components: {
-    HomeComponent,
+    SubjectCard,
+  },
+  data() {
+    return {
+      subjects: [
+        {
+          id: 1,
+          title: "Fullstack",
+          code: "IDATT2105",
+          queueAmount: 2,
+          eta: "Estimert ventetid, kjempelenge",
+          message:
+            "Dette er et fag! Du kan foreløpig ikke stille deg i kø, men dette\n" +
+            "              kommer snart.",
+        },
+        {
+          id: 2,
+          title: "Nettverksprog",
+          code: "IDATT2104",
+          queueAmount: 2,
+          eta: "Estimert ventetid, kjempelenge",
+          message:
+            "Dette er et fag! Du kan foreløpig ikke stille deg i kø, men dette\n" +
+            "              kommer snart.",
+        },
+      ],
+    };
   },
 };
 </script>
-
