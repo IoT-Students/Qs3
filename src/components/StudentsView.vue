eab2906e204daf59939524ed18f4ff7cffe7f970
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-31 13:12:19 +0200

    Nav buttons reflect on the role of the user

---
+++ src/components/StudentsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <StudentsCard
+    v-for="student in students"
+    :key="student.userID"
+    :student="student"
+  ></StudentsCard>
+</template>
+
+<script>
+import StudentsCard from "@/components/StudentsCard";
+export default {
+  name: "StudentsView",
+  components: { StudentsCard },
+  computed: {
+    students() {
+      return this.$store.state.subjectStudents;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
