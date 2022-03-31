2b3c63833e71c22e78274278c986e999345b07e1
Marius Klemp Petersen <mariuskp@stud.ntnu.no>
2022-03-31 15:49:03 +0200

    Updated design login

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="loginContainer">
-    <h1 id="loginTitle">Please login!</h1>
-    <form class="login" @submit.prevent="SignIn">
-      <fieldset>
-        <BaseInput
-          id="username"
-          class="baseInput"
-          v-model="userInfo.username"
-          label="Username"
-          type="text"
-        />
-      </fieldset>
-      <fieldset>
-        <BaseInput
-          id="password"
-          class="baseInput"
-          v-model="userInfo.password"
-          label="Password"
-          type="text"
-        />
-      </fieldset>
-      <BaseButton type="submit" class="mybtn" something="else">
-        Sign in
-      </BaseButton>
-    </form>
-    <label id="loginstatusLabel"></label>
+  <div class="loginContainer border ">
+
+    <div class="header">
+      <div>QS<span>3</span></div>
+    </div>
+
+    <div class="loginFunction">
+      <h1 id="loginTitle">Login to QS3</h1>
+      <form class="login" @submit.prevent="SignIn">
+        <fieldset>
+          <p id="username">Username</p>
+          <BaseInput
+              id="username"
+              class="baseInput"
+              v-model="userInfo.username"
+              type="text"
+          />
+        </fieldset>
+        <fieldset>
+          <p id="password">Password</p>
+          <BaseInput
+              id="password"
+              class="baseInput"
+              v-model="userInfo.password"
+              type="text"
+          />
+        </fieldset>
+        <BaseButton type="submit" class="mybtn" something="else">
+          Sign in
+        </BaseButton>
+      </form>
+    </div>
+
   </div>
 </template>
 
@@ -85,6 +92,42 @@ export default {
 </script>
 
 <style scoped>
+.loginContainer{
+  display: flex;
+  flex-direction: column;
+  row-gap: 2px;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 15%;
+  width: 300px;
+  background-color: darkgray;
+
+
+}
+.border {
+  border:1px solid;
+  border-color:black;
+  border-radius: 25px;
+}
+
+.header div{
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 60px;
+  font-weight: 500;
+}
+#username{
+  color: black;
+}
+#password{
+  color: black;
+
+}
+
+.header div span{
+  color: #5379fa !important;
+}
+
 fieldset {
   border: 0;
   margin: 0;
@@ -93,151 +136,40 @@ fieldset {
 mybtn,
 label,
 input,
-textarea {
-  display: inline-flex;
-  font-family: "Open sans", sans-serif;
-  font-size: 100%;
-  line-height: 1.15;
-  margin: 0;
-}
-mybtn,
-input {
-  overflow: visible;
-}
-mybtn,
-select {
-  text-transform: none;
-}
-mybtn,
-[type="button"],
-[type="reset"],
-[type="submit"] {
-  -webkit-appearance: none;
-}
-mybtn::-moz-focus-inner,
-[type="button"]::-moz-focus-inner,
-[type="reset"]::-moz-focus-inner,
-[type="submit"]::-moz-focus-inner {
-  border-style: none;
-  padding: 0;
-}
-mybtn:-moz-focusring,
-[type="button"]:-moz-focusring,
-[type="reset"]:-moz-focusring,
-[type="submit"]:-moz-focusring {
-  outline: 2px solid #39b982;
-}
-label {
-  color: rgba(0, 0, 0, 0.5);
-  font-weight: 700;
-}
+optgroup,
+select,
+
 input,
-textarea {
-  box-sizing: border-box;
-  border: solid 1px rgba(0, 0, 0, 0.4);
-}
-input.error,
-select.error {
-  margin-bottom: 0;
-}
-input + p.errorMessage {
-  margin-bottom: 24px;
-}
-textarea {
-  width: 100%;
-  overflow: auto;
-  font-size: 20px;
-}
-[type="checkbox"],
-[type="radio"] {
-  box-sizing: border-box;
-  padding: 0;
-  margin-right: 0.5rem;
-}
-[type="number"]::-webkit-inner-spin-button,
-[type="number"]::-webkit-outer-spin-button {
-  height: auto;
-}
-[type="search"] {
-  -webkit-appearance: textfield;
-  outline-offset: -2px;
-}
-[type="search"]::-webkit-search-decoration {
-  -webkit-appearance: none;
-}
-[type="text"],
-[type="number"],
-[type="search"],
-[type="password"] {
-  height: 52px;
-  width: 40%;
-  padding: 0 10px;
-  font-size: 20px;
+[type="text"] {
+  height: 100px;
+  width: 25%;
+  padding: 0 5px;
+  font-size: 18px;
 }
 [type="text"]:focus,
-[type="number"]:focus,
-[type="search"]:focus,
 [type="password"]:focus {
   border-color: #39b982;
 }
-::-webkit-file-upload-button {
-  -webkit-appearance: button;
-  font: inherit;
-}
+
 [hidden] {
   display: none;
 }
 .error {
   border: 1px solid red;
 }
-select {
-  width: 100%;
-  height: 52px;
-  padding: 0 24px 0 10px;
-  vertical-align: middle;
-  background: #fff
-    url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E")
-    no-repeat right 12px center;
-  background-size: 8px 10px;
-  border: solid 1px rgba(0, 0, 0, 0.4);
-  border-radius: 0;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-}
-select:focus {
-  border-color: #39b982;
-  outline: 0;
-}
-select:focus::ms-value {
-  color: #000;
-  background: #fff;
-}
-select::ms-expand {
-  opacity: 0;
-}
-.field {
-  margin-bottom: 24px;
-}
-.error {
-  border: 1px solid red;
-}
-.errorMessage {
-  color: red;
-}
+
 .mybtn {
-  display: inline-flex;
   align-items: center;
   justify-content: space-between;
-  height: 20px;
+  width: 170px;
+  height: 15px;
   padding: 20px;
-  margin: 10px;
-  background-color: lightgreen;
-  border-radius: 6px;
+  background-color: cornflowerblue;
+  border-radius: 4px;
   text-align: center;
   font-weight: 600;
-  white-space: nowrap;
   transition: all 0.2s linear;
+  margin: 0 auto;
 }
 .mybtn:hover {
   -webkit-transform: scale(1.02);
@@ -258,7 +190,4 @@ select::ms-expand {
   box-shadow: none;
 }
 
-#header {
-  color: #39b982;
-}
 </style>
